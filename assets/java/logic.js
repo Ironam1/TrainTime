47afe772c5aa76bcef405db4e0f7f1228a3352ef
Ironam1 <ironam1@icloud.com>
2019-06-08 21:17:47 -0400

    begining logic works. user is able to input data and amend table. math works for generating arrival time and time till next train.

--- assets/java/logic.js
+++ assets/java/logic.js
@@ -1,20 +1,49 @@
-var timeNow = moment().format("HH:mm");
-console.log(timeNow);
-$(document).ready(function(){
-    $("#submit").on("click", function(e){
-        e.preventDefault();
-        var trainName = $("#NewTrainName").val().trim();
-        var destination = $("#NewDestination").val().trim();
-        var frequency = $("#NewFrequency").val().trim();
-        var firstDepart = $("#NewFirstDeparture").val();
-        console.log(trainName);
-        console.log(destination);
-        console.log(frequency);
-        console.log(firstDepart);
-        $(".table").append("<tr/", {
-            td: trainName,
-            td: destination,
-            td: frequency,
-            })
-    })
-})
+$(document).ready(function() {
+  var timeNow = moment().format("HH:mm");
+  console.log(timeNow);
+  function addTrain() {
+    $("#submit").on("click", function(e) {
+      e.preventDefault();
+      var trainName = $("#NewTrainName")
+        .val()
+        .trim();
+      var destination = $("#NewDestination")
+        .val()
+        .trim();
+      var frequency = $("#NewFrequency")
+        .val()
+        .trim();
+      var firstDepart = $("#NewFirstDeparture")
+        .val()
+        .trim();
+      var firstTime = moment(firstDepart, "HH:mm").subtract(1, "years");
+      var diffTime = moment().diff(moment(firstTime), "minutes");
+      console.log("time difference " + diffTime);
+      var tRemaining = diffTime % frequency;
+      console.log("remainder " + tRemaining);
+      var tillNextTrain = frequency - tRemaining;
+      console.log("NEXT " + tillNextTrain);
+      var nextArr = moment().add(tillNextTrain, "minutes");
+      console.log("ARRIVAL " + moment(nextArr).format("hh:mm"));
+      console.log(trainName);
+      console.log(destination);
+      console.log(firstDepart);
+      console.log(frequency);
+      $("table")
+        .find("tbody")
+        .append(
+          [
+            "<tr>",
+            "<td>" + trainName + "</td>",
+            "<td>" + destination + "</td>",
+            "<td>" + frequency + "</td>",
+            "<td>" + nextArr + "</td>",
+            "<td>" + tillNextTrain + "</td>",
+            "</tr>"
+          ].join("")
+        );
+    });
+    function getArr() {}
+  }
+  addTrain();
+});
