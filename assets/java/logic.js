c4e6b51e42ab8aa462827530418d2b17b5cf7d47
Ironam1 <ironam1@icloud.com>
2019-06-11 22:04:04 -0400

    added some styling. not sure if authentication is correct. have it commented out.

--- assets/java/logic.js
+++ assets/java/logic.js
@@ -14,7 +14,36 @@ $(document).ready(function() {
   firebase.initializeApp(firebaseConfig);
 
   var database = firebase.database();
-
+  // var provider = new firebase.auth.GithubAuthProvider();
+  // firebase
+  //   .auth()
+  //   .signInWithPopup(provider)
+  //   .then(function(result) {
+  //     // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+  //     var token = result.credential.accessToken;
+  //     // The signed-in user info.
+  //     var user = result.user;
+  //     // ...
+  //   })
+  //   .catch(function(error) {
+  //     // Handle Errors here.
+  //     var errorCode = error.code;
+  //     var errorMessage = error.message;
+  //     // The email of the user's account used.
+  //     var email = error.email;
+  //     // The firebase.auth.AuthCredential type that was used.
+  //     var credential = error.credential;
+  //     // ...
+  //   });
+  // firebase
+  //   .auth()
+  //   .signOut()
+  //   .then(function() {
+  //     // Sign-out successful.
+  //   })
+  //   .catch(function(error) {
+  //     // An error happened.
+  //   });
   database.ref().on("child_added", function(childSnapshot) {
     console.log(childSnapshot.val().trainName);
     console.log(childSnapshot.val().destination);
@@ -46,7 +75,7 @@ $(document).ready(function() {
         ].join("")
       );
   });
-  
+
   $("#submit").on("click", function(e) {
     e.preventDefault();
     var trainName = $("#NewTrainName")
